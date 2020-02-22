61ad3878da71996553c5252cfd8a70746a527060
Sagar Makar <46819032+sagarmakar@users.noreply.github.com>
2020-02-22 17:02:40 +0530

    Create ServerDelegate.java

---
+++ ServerDelegate.java
@@ -0,0 +1,57 @@
+
+package remoteclient;
+
+import java.awt.Robot;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+class ServerDelegate extends Thread {
+
+    Socket socket = null;
+    Robot robot = null;
+    boolean continueLoop = true;
+
+    public ServerDelegate(Socket socket, Robot robot) {
+        this.socket = socket;
+        this.robot = robot;
+        start(); 
+    }
+
+    public void run(){
+        Scanner scanner = null;
+        try {
+
+            System.out.println("Preparing InputStream");
+            scanner = new Scanner(socket.getInputStream());
+
+            while(continueLoop){
+  
+                System.out.println("Waiting for command");
+                int command = scanner.nextInt();
+                System.out.println("New command: " + command);
+                switch(command){
+                    case -1:
+                        robot.mousePress(scanner.nextInt());
+                    break;
+                    case -2:
+                        robot.mouseRelease(scanner.nextInt());
+                    break;
+                    case -3:
+                        robot.keyPress(scanner.nextInt());
+                    break;
+                    case -4:
+                        robot.keyRelease(scanner.nextInt());
+                    break;
+                    case -5:
+                        robot.mouseMove(scanner.nextInt(), scanner.nextInt());
+                    break;
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
